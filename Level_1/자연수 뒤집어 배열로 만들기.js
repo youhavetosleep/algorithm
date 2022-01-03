95001b25459684f14d9758353cc79ba1ad7b4771
youhavetosleep <qksekf4@gmail.com>
2022-01-03 10:28:46 +0900

    자연수 뒤집어 배열로 만들기

---
+++ Level_1/자연수 뒤집어 배열로 만들기.js
@@ -0,0 +1,11 @@
+// 문제
+// 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요.
+// 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+// ---- 풀이
+function solution(n) {
+  let str = String(n);
+  let splited = str.split("").reverse();
+  let result = splited.map((el) => Number(el));
+  return result;
+}
