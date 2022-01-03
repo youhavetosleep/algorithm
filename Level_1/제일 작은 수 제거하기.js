9113e67c37b28d22197ac50aebf35a98042c545a
youhavetosleep <qksekf4@gmail.com>
2022-01-03 10:25:48 +0900

    정수 제곱근 판별

---
+++ Level_1/제일 작은 수 제거하기.js
@@ -0,0 +1,21 @@
+// 문제
+// 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요.
+// 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
+// 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+// ---- 풀이
+function solution(arr) {
+  if (arr.length === 1) {
+    return [-1];
+  }
+
+  let min = Math.min.apply(null, arr);
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== min) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
