2219f9f8ccfcbfb1c478d8822a9889be6783f495
youhavetosleep <qksekf4@gmail.com>
2022-02-04 10:11:33 +0900

    문자열 압축

---
+++ Inflearn/section-3/문자열 압축.js
@@ -0,0 +1,42 @@
+// 문제
+// 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는 문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하시오. 단 반복횟수가 1인 경우 생략합니다.
+
+// ▣ 입력설명
+// 첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
+// ▣ 출력설명
+// 첫 줄에 압축된 문자열을 출력한다.
+
+// ---- 풀이
+function solution(s) {
+  let num = 1;
+  let result = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      num++;
+    } else {
+      result += s[i];
+      if (num !== 1) {
+        result += num;
+      }
+      num = 1;
+    }
+  }
+  return result;
+}
+
+// reference
+function solution(s) {
+  let answer = "";
+  let cnt = 1;
+  s = s + " ";
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] === s[i + 1]) cnt++;
+    else {
+      answer += s[i];
+      if (cnt > 1) answer += String(cnt);
+      cnt = 1;
+    }
+  }
+  return answer;
+}
